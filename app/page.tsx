5930c0d70a5c196980ea470d19bf5b46a42c7236
naufal <askpradana@gmail.com>
2024-02-27 15:52:49 +0700

    style: add shadcn for ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Bantuan from "./bantuan";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
 
 	return (
 		<main>
+			<Button>Read a Books</Button>
 			<Bantuan />
 		</main>
 	);
