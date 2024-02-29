ad68636247ec16b45f829f833b74423907434062
naufal <askpradana@gmail.com>
2024-02-29 15:32:54 +0700

    feat: add radix icon and fact pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Bantuan from "./bantuan";
+import Fact from "./components/fact";
 import Header from "./components/header";
 
-
 export default function Home() {
-
 	return (
 		<main>
 			<Header />
-			<Bantuan />
+			{/* <Bantuan /> */}
+			<Fact />
 		</main>
 	);
 }
